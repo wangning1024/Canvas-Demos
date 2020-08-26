ebdf352ec77f275aa1b41cb88ae172d44d7a85e5
wangning <1151089590@qq.com>
2020-08-26 18:03:50 +0800

    提交文件

--- 2-28.js
+++ 2-28.js
@@ -19,6 +19,60 @@ let canvas = document.getElementById('canvas'),
     editing = false,
     polygons = [];
 
+let Point = function (x, y) {
+    this.x = x;
+    this.y = y;
+};
+
+let Polygon = function (centerX, centerY, radius, sides, startAngle, strokeStyle, fillStyle, filled) {
+    this.x = centerX;
+    this.y = centerY;
+    this.radius = radius;
+    this.sides = sides;
+    this.startAngle = startAngle;
+    this.strokeStyle = strokeStyle;
+    this.fillStyle = fillStyle;
+    this.filled = filled;
+};
+Polygon.prototype = {
+    getPoints: function () {
+        let points = [],
+            angle = this.startAngle || 0;
+        for (let i = 0; i < this.sides; ++i) {
+            points.push(new Point(this.x + this.radius * Math.sin(angle), this.y - this.radius * Math.cos(angle)));
+            angle += 2 * Math.PI / this.sides;
+        }
+        return points;
+    },
+    createPath: function (context) {
+        let points = this.getPoints();
+        context.beginPath();
+        context.moveTo(points[0].x, points[0].y);
+        for (let i = 1; i < this.sides; ++i) {
+            context.lineTo(points[i].x, points[i].y);
+        }
+        context.closePath();
+    },
+    stroke: function (context) {
+        context.save();
+        this.createPath(context);
+        context.strokeStyle = this.strokeStyle;
+        context.stroke();
+        context.restore();
+    },
+    fill: function (context) {
+        context.save();
+        this.createPath(context);
+        context.fillStyle = this.fillStyle;
+        context.fill();
+        context.restore();
+    },
+    move: function (x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 function saveDrawingSurface() {
     drawingSurfaceImageData = context.getImageData(0, 0, canvas.width, canvas.height);
 }
