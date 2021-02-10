d6b17d57b043f056d7d0941ed037a1e7ea3b652e
wangning <1151089590@qq.com>
2021-02-10 15:18:54 +0800

    提交文件

--- chapter1/1-3.js
+++ chapter1/1-3.js
@@ -2,7 +2,25 @@ let canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d');
 
 context.font = '38pt Arial';
-context.fillStyle = 'cornflowerblue';
-context.strokeStyle = 'blue';
+context.fillStyle = '#ed5a65';
+context.strokeStyle = '#c04851';
 context.fillText('Hello Canvas', canvas.width / 2 - 150, canvas.height / 2 + 15);
 context.strokeText('Hello Canvas', canvas.width / 2 - 150, canvas.height / 2 + 15);
+
+let canvas2 = document.getElementById('canvas2'),
+    context2 = canvas2.getContext('2d');
+
+context2.font = '38pt Arial';
+context2.fillStyle = '#ed5a65';
+context2.strokeStyle = '#c04851';
+context2.fillText('Hello Canvas', canvas.width / 2 - 150, canvas.height / 2 + 15);
+context2.strokeText('Hello Canvas', canvas.width / 2 - 150, canvas.height / 2 + 15);
+
+let canvas3 = document.getElementById('canvas3'),
+    context3 = canvas3.getContext('2d');
+
+context3.font = '38pt Arial';
+context3.fillStyle = '#ed5a65';
+context3.strokeStyle = '#c04851';
+context3.fillText('Hello Canvas', canvas.width / 2 - 150, canvas.height / 2 + 15);
+context3.strokeText('Hello Canvas', canvas.width / 2 - 150, canvas.height / 2 + 15);
