3bee8abb7d8ea3250ace4b470c72b8c6aa19fb0a
wangning <1151089590@qq.com>
2021-03-01 17:35:26 +0800

    提交文件

--- chapter4/4-9.js
+++ chapter4/4-9.js
@@ -7,6 +7,8 @@ let mousedown = {};
 let rubberbandRectangle = {};
 
 let isDragging = false;
+// 图像数据
+let imageData = undefined;
 
 // 开始绘制选取矩形
 function rubberbandStart(x, y) {
@@ -19,6 +21,62 @@ function rubberbandStart(x, y) {
     // 标识开始绘制
     isDragging = true;
 }
+
+// 绘制橡皮筋矩形
+function rubberbandStretch(x, y) {
+    if (rubberbandRectangle.width > 2*context.lineWidth &&
+        rubberbandRectangle.height > 2*context.lineWidth) {
+        if (imageData !== undefined) {
+            restoreRubberbandPixels();
+        }
+    }
+    // 设置橡皮筋矩形坐标
+    setRubberbandRectangle(x, y);
+
+    if (rubberbandRectangle.width > 2*context.lineWidth &&
+        rubberbandRectangle.height > 2*context.lineWidth) {
+        updateRubberband();
+    }
+}
+
+// 设置橡皮筋矩形坐标
+function setRubberbandRectangle(x, y) {
+    rubberbandRectangle.left = Math.min(x, mousedown.x);
+    rubberbandRectangle.top  = Math.min(y, mousedown.y);
+    rubberbandRectangle.width  = Math.abs(x - mousedown.x);
+    rubberbandRectangle.height = Math.abs(y - mousedown.y);
+}
+
+function restoreRubberbandPixels() {
+    var deviceWidthOverCSSPixels = imageData.width / rubberbandRectangle.width,
+        deviceHeightOverCSSPixels = imageData.height / rubberbandRectangle.height;
+
+    context.putImageData(imageData,
+        rubberbandRectangle.left * deviceWidthOverCSSPixels,
+        rubberbandRectangle.top * deviceHeightOverCSSPixels);
+}
+
+
+function updateRubberband() {
+    captureRubberbandPixels();
+    drawRubberband();
+}
+function captureRubberbandPixels() {
+    imageData = context.getImageData(rubberbandRectangle.left,
+        rubberbandRectangle.top,
+        rubberbandRectangle.width,
+        rubberbandRectangle.height);
+}
+function drawRubberband() {
+    context.strokeRect(rubberbandRectangle.left + context.lineWidth,
+        rubberbandRectangle.top + context.lineWidth,
+        rubberbandRectangle.width - 2*context.lineWidth,
+        rubberbandRectangle.height - 2*context.lineWidth);
+}
+
+
+
+
 // 鼠标点击
 canvas.onmousedown = function (ev) {
     let loc = windowToCanvas(canvas, ev.clientX, ev.clientY);
@@ -28,7 +86,10 @@ canvas.onmousedown = function (ev) {
 };
 // 鼠标移动
 canvas.onmousemove = function (e) {
-
+    if (isDragging) {
+        let loc = windowToCanvas(canvas, ev.clientX, ev.clientY);
+        rubberbandStretch(loc.x, loc.y);
+    }
 
 }
 
